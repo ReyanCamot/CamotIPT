54a2ddbcbe70b7a4c526e69cf9483dec618a9227
ReyanCamot <143674806+ReyanCamot@users.noreply.github.com>
2026-01-29 11:12:15 +0800

    Delete IPT102CamotData directory

--- IPT102CamotData/Stored Procedure/DeleteMovie.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[DeleteMovie]
-    @Id INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    DELETE FROM [dbo].[Movie]
-    WHERE Id = @Id;
-END
--- IPT102CamotData/Tables/Movie.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Movie]
-(
-    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    [Title] NVARCHAR(200) NOT NULL,
-    [ReleaseYear] INT NULL,
-    [RuntimeMinutes] INT NULL,
-    [Rating] DECIMAL(3,1) NULL
-)
